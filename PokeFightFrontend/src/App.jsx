2e28793ffd2e3646db771823f16bfb8e63aea415
Antonio Bjelobrk <bjelobrka@gmail.com>
2024-02-12 17:05:14 +0100

    added pokedex

--- PokeFightFrontend/src/App.jsx
+++ PokeFightFrontend/src/App.jsx
@@ -5,57 +5,57 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./Styles/App.css";
 import LandingPage from "./Components/LandingPage.jsx";
 import PokemonIdPage from "./Components/PokemonIdPage.jsx";
-import PokemonInfoPage from "./Components/PokemonInfoPage.jsx";
+import Pokedex from "./Components/Pokedex.jsx";
 import EntryPage from "./Components/EntryPage.jsx";
 import MediaPlayer from "./Components/MediaPlayer.jsx";
 
 function App() {
 
-    const [playing, setPlaying] = useState(false);
-    const [showMediaPlayer, setShowMediaPlayer] = useState(false);
-    const audioRef = useRef(null);
+  const [playing, setPlaying] = useState(false);
+  const [showMediaPlayer, setShowMediaPlayer] = useState(false);
+  const audioRef = useRef(null);
 
-    const [pokedexAudioPlaying, setPokedexAudioPlaying] = useState(false);
-    const pokedexAudioRef = useRef(null);
+  const [pokedexAudioPlaying, setPokedexAudioPlaying] = useState(false);
+  const pokedexAudioRef = useRef(null);
 
-    const toggleAudio = () => {
-        if(playing) {
-            audioRef.current.volume = 0;
-            setPlaying(false);
-        } else {
-            audioRef.current.volume = 0.2;
-            audioRef.current.play();
-            setPlaying(true);
-        };
+  const toggleAudio = () => {
+    if (playing) {
+      audioRef.current.volume = 0;
+      setPlaying(false);
+    } else {
+      audioRef.current.volume = 0.2;
+      audioRef.current.play();
+      setPlaying(true);
     };
+  };
 
-    const startAudio = () => {
-      setShowMediaPlayer(true);
-      if(!playing) {
-        audioRef.current.volume = 0.2;
-        audioRef.current.play();
-        setPlaying(true);
-      };
+  const startAudio = () => {
+    setShowMediaPlayer(true);
+    if (!playing) {
+      audioRef.current.volume = 0.2;
+      audioRef.current.play();
+      setPlaying(true);
     };
+  };
 
-    return (
-      <BrowserRouter>
-        <MediaPlayer 
-        audioRef={audioRef} 
-        toggleAudio={toggleAudio} 
+  return (
+    <BrowserRouter>
+      <MediaPlayer
+        audioRef={audioRef}
+        toggleAudio={toggleAudio}
         playing={playing}
         pokedexAudioRef={pokedexAudioRef}
-        pokedexAudioPlaying={pokedexAudioPlaying} 
+        pokedexAudioPlaying={pokedexAudioPlaying}
         showMediaPlayer={showMediaPlayer} />
-        <Routes>
-          <Route path="/" element={<EntryPage toggleAudio={startAudio}  />} />
-          <Route path="/home" element={<LandingPage />}/>
-          <Route path="/pokemon/:id" element={<PokemonIdPage />} />
-          <Route path="/pokemon/:id/info" element={<PokemonInfoPage />} />
+      <Routes>
+        <Route path="/" element={<EntryPage toggleAudio={startAudio} />} />
+        <Route path="/home" element={<LandingPage />} />
+        <Route path="/pokemon/" element={<Pokedex />} />
+        <Route path="/pokemon/:id" element={<PokemonIdPage />} />
 
-        </Routes>
-      </BrowserRouter>
-    );
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- PokeFightFrontend/src/Components/PokemonInfoPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PokemonInfoPage = () => {
-  return (
-    <div>PokemonInfoPage</div>
-  )
-}
-
-export default PokemonInfoPage;
