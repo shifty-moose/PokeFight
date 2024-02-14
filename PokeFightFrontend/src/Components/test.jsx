2de49790252f6c1354741c7f0d1f783062f91e49
Antonio Bjelobrk <bjelobrka@gmail.com>
2024-02-15 00:36:27 +0100

    added the basic fighting logic

--- PokeFightFrontend/src/Components/test.jsx
+++ PokeFightFrontend/src/Components/test.jsx
@@ -7,17 +7,15 @@ import pokemonAPI from '../../pokemonAPI';
 
 const Fight = () => {
 
-
-
     const [spriteOne, animateSpriteOne] = useAnimate();
     const [spriteTwo, animateSpriteTwo] = useAnimate();
     const [attackMessage, setAttackMessage] = useState("");
     const [isAttacking, setIsAttacking] = useState(false);
     const [pkm2, setpkm2] = useState(true);
+    const [pk1, setpk1] = useState({});
     const [loading, setLoading] = useState(true);
     const [pokemon1HPWidth, setPokemon1HPWidth] = useState(100);
     const [pokemon2HPWidth, setPokemon2HPWidth] = useState(100);
-
 
     const { getPokemon } = pokemonAPI();
 
@@ -31,7 +29,7 @@ const Fight = () => {
     const fetchPokemon = async () => {
         try {
             setLoading(true);
-            const { name: { english: name }, sprites: { front_default: sprite }, base: { Attack: attack }, base: { HP: hp }, base: { Defense: defense }, base: { Speed: speed } } = await getPokemon(319);
+            const { name: { english: name }, sprites: { front_default: sprite }, base: { Attack: attack }, base: { HP: hp }, base: { Defense: defense }, base: { Speed: speed } } = await getPokemon(id);
             setPokemon2({ name, speed, sprite, attack, defense, hp });
             console.log(pokemon2)
 
@@ -41,29 +39,26 @@ const Fight = () => {
             setLoading(false);
         }
     };
-    const [initialPokemon1HP, setInitialPokemon1HP] = useState();
 
     useEffect(() => {
         fetchPokemon();
-        setInitialPokemon1HP(pokemon1.hp);
-
     }, [])
 
     const [pokemon1, setPokemon1] = useState({
         name: 'PIKACHU',
         type: 'Electric',
-        hp: 110,
+        hp: 100,
         attack: 55,
         defense: 40,
         spAttack: 50,
         spDefense: 50,
-        speed: 100,
+        speed: 0,
         sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/25.png'
     });
 
     const [pokemon2, setPokemon2] = useState({});
 
-    ;
+
 
 
     const spirit1Attack = () => {
@@ -80,7 +75,12 @@ const Fight = () => {
         animateSpriteTwo(spriteTwo.current, { x: 0, y: 60, transition: { duration: 0.5 } });
     }
 
-    const dodgeProbability = 0.5;
+    const spirit1Faint = () => {
+        animateSpriteTwo(spriteTwo.current, { x: 0, y: 60, transition: { duration: 0.5 } });
+    }
+
+
+    const dodgeProbability = 0.2;
     const criticalHitProbability = 0.1;
 
     const isDodged = () => Math.random() < dodgeProbability;
@@ -91,29 +91,26 @@ const Fight = () => {
         const dodge1 = isDodged() ? 0 : 1;
 
 
-        const firstAttacker = pokemon1;
-        const secondAttacker = pokemon2;
-
+        //calculates which pokemon attacks first
+        const firstAttacker = pokemon1.speed >= pokemon2.speed ? pokemon1 : pokemon2;
+        const secondAttacker = firstAttacker === pokemon1 ? pokemon2 : pokemon1;
+        // Calculate damage for the first attacker
         const damage = Math.round(20 * (firstAttacker.attack / secondAttacker.defense) * damageMultiplier1 * dodge1);
-        console.log(damage)
         const remainingHP2 = Math.max(secondAttacker.hp - damage, 0);
-        console.log(remainingHP2)
 
-        const remainingHPPercentage2 = Math.max(0, Math.min(100, (remainingHP2 / secondAttacker.hp) * 100));
+        const remainingHPPercentage2 = remainingHP2;
         setIsAttacking(true);
 
 
         // Update state for the second attacker (opponent)
         setPokemon2({ ...secondAttacker, hp: remainingHP2 });
-        if (damage > 1) {
-            setPokemon2HPWidth(remainingHPPercentage2);
-        }
+        setPokemon2HPWidth(remainingHPPercentage2);
         setAttackMessage(`${firstAttacker.name} USED TACKLE`)
         setTimeout(() => {
-            if (damageMultiplier1 > 1.1 && damage > 0) {
+            if (damageMultiplier1 > 1.1 && damage1 > 0) {
                 setAttackMessage("It was critical")
-            } else if (damage === 0) {
-                setAttackMessage(`${firstAttacker.name} missed the attack`)
+            } else if (damage1 == 0) {
+                setAttackMessage(`${pokemon1.name} missed the attack"`)
             }
         }, 1000);
         spirit1Attack();
@@ -124,19 +121,15 @@ const Fight = () => {
                 spirit2Faint();
                 setAttackMessage(`Enemy's ${secondAttacker.name} has fainted! You get some HP back`)
 
-                setPokemon1({ ...firstAttacker, hp: firstAttacker.hp + 20 });
 
 
-
-                setTimeout(async () => {
-
+                setTimeout(() => {
                     setpkm2(false);
                     setTimeout(() => {
                         setAttackMessage(`Oppenent is about the send new pokemon out.`)
                     }, 2500);
                 }, 100);
             }, 2500);
-
             return 0;
         }
 
@@ -145,21 +138,20 @@ const Fight = () => {
             const damageMultiplier2 = isCriticalHit() ? 1.5 : 1;
             const dodge2 = isDodged() ? 0 : 1;
             // Calculate damage I need to adjust it still
-            const damage2 = Math.round(20 * (secondAttacker.attack / firstAttacker.defense) * damageMultiplier2 * dodge2);
+            const damage2 = Math.round(20 * (secondAttacker.attack / secondAttacker.defense) * damageMultiplier2 * dodge2);
             const remainingHP1 = Math.max(firstAttacker.hp - damage2, 0);
-            const remainingHPPercentage1 = Math.max(0, Math.min(100, (remainingHP1 / firstAttacker.hp) * 100));
+            const remainingHPPercentage1 = remainingHP1;
+            console.log(damage2)
 
             setPokemon1({ ...firstAttacker, hp: remainingHP1 });
-            if (damage2 > 1) {
-                setPokemon1HPWidth(remainingHPPercentage1);
-            }
+            setPokemon1HPWidth(remainingHPPercentage1);
             setAttackMessage(`${secondAttacker.name} USED TACKLE`)
             spirit2Attack();
             setTimeout(() => {
                 if (damageMultiplier2 > 1.1 & damage2 > 0) {
                     setAttackMessage("It was critical")
                 } else if (damage2 == 0) {
-                    setAttackMessage(`${secondAttacker.name} missed the attack`)
+                    setAttackMessage(`${pokemon2.name} missed the attack`)
                 }
             }, 1000);
 
@@ -178,10 +170,7 @@ const Fight = () => {
 
         }, 3000);
 
-
-
-
-    }
+    };
 
     if (loading) {
         return <div>Loading...</div>;
@@ -232,7 +221,7 @@ const Fight = () => {
                                         <h5>HP:</h5>
                                         <div className='hpBar'>
                                             <div className='hpBarFill' style={{ width: `${pokemon1HPWidth}%`, transition: 'width 1.5s ease-in-out' }}></div>
-                                            <h5>{`${initialPokemon1HP} / ${pokemon1.hp}`}</h5>
+                                            <h5>{pokemon1.hp + "/" + pokemon1.hp}</h5>
                                         </div>
                                     </div>
                                 </div>
